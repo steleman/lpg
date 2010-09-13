c8068d12145a9b4514e1e1e0c7b411d10d579c07
pgcharles <>
2010-09-13 14:53:59 +0000

    New windows executable for 2.0.20

--- lpg.generator.cpp/src/make-Win32-x86-MSNET.bat
+++ lpg.generator.cpp/src/make-Win32-x86-MSNET.bat
@@ -2,5 +2,5 @@ REM Build generator executable using Microsoft .Net Studio compiler for Win32-x8
 REM TODO: Turn this into a proper Makefile usable by cygwin/gnu make.
 
 cl /Zi /EHsc /DWIN32 /Felpg-win32_x86 *.cpp
-move lpg-win32_x86.exe lpg-win32_x86
+move /Y lpg-win32_x86.exe ..\..\lpg.generator.win32_x86\lpgexe\lpg-win32_x86
 
