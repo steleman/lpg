4633152f08d3f3dac096a1815927e6b8367bd619
pgcharles <>
2011-01-06 20:39:06 +0000

    Add implementation for iterator() and listIterator()

--- lpg.runtime.java/src/lpg/runtime/SegmentedTuple.java
+++ lpg.runtime.java/src/lpg/runtime/SegmentedTuple.java
@@ -1,10 +1,11 @@
 package lpg.runtime;
 
 import java.util.Collection;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
-
+import java.util.NoSuchElementException;
 
 //
 // This Tuple class can be used to construct a dynamic
@@ -17,6 +18,8 @@ import java.util.ListIterator;
 //
 public class SegmentedTuple <T> implements List<T>
 {
+    private int modCount = 0;
+    
     private int top,
                 size;
 
@@ -82,6 +85,7 @@ public class SegmentedTuple <T> implements List<T>
     public void resize() { resize(0); }
     public void resize(int n)
     {
+        modCount++; // tuple structure  modified!
         //
         // If array did not previously contain enough space, allocate
         // the necessary additional space. Otherwise, if the array had
@@ -105,8 +109,7 @@ public class SegmentedTuple <T> implements List<T>
     // indicates that the size should be reset to 0.
     //
     public void reset() { reset(0); }
-    public void reset(int n)
-    {
+    public void reset(int n) {
         top = n;
     }
 
@@ -159,6 +162,7 @@ public class SegmentedTuple <T> implements List<T>
     //
     public boolean add(T element)
     {
+        modCount++; // tuple structure  modified!
         int i = NextIndex();
         base[i >> log_blksize][i % (1 << log_blksize)] = element;
         
@@ -269,18 +273,100 @@ public class SegmentedTuple <T> implements List<T>
     }
 
     public Iterator<T> iterator() {
-        throw new UnsupportedOperationException();
+        return new Itr();    
     }
 
     public ListIterator<T> listIterator() {
-        throw new UnsupportedOperationException();
+        return listIterator(0);
     }
 
     public ListIterator<T> listIterator(int index) {
-        throw new UnsupportedOperationException();
+        if (index<0 || index>size())
+            throw new IndexOutOfBoundsException("Index: " + index);
+        return new ListItr(index);
     }
 
     public List<T> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
+    }
+
+    /**
+     * This is a duplication of the private class by the same name in java.util.AbstractList.
+     */
+    private class Itr implements Iterator<T> {
+        int cursor = 0;
+
+        int lastRet = -1;
+
+        int expectedModCount = modCount;
+
+        public boolean hasNext() {
+            return cursor != size();
+        }
+
+        public T next() {
+            checkForComodification();
+            try {
+                T next = get(cursor);
+                lastRet = cursor++;
+                return next;
+            }
+            catch(IndexOutOfBoundsException e) {
+                 checkForComodification();
+                 throw new NoSuchElementException();
+            }
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        final void checkForComodification() {
+             if (modCount != expectedModCount)
+                  throw new ConcurrentModificationException();
+        }
+    }
+
+    /**
+     * This is a duplication of the private class by the same name in java.util.AbstractList.
+     */
+    private class ListItr extends Itr implements ListIterator<T> {
+        ListItr(int index) {
+            cursor = index;
+        }
+
+        public boolean hasPrevious() {
+            return cursor != 0;
+        }
+
+        public T previous() {
+            checkForComodification();
+            try {
+                int i = cursor - 1;
+                T previous = get(i);
+                lastRet = cursor = i;
+                return previous;
+            }
+            catch(IndexOutOfBoundsException e) {
+                checkForComodification();
+                throw new NoSuchElementException();
+            }
+        }
+
+        public int nextIndex() {
+            return cursor;
+        }
+
+        public int previousIndex() {
+            return cursor - 1;
+        }
+
+        public void set(T o) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void add(T o) {
+            throw new UnsupportedOperationException();
+        }
     }
 };
