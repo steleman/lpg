659293d49234532108c00b8e3e8a9367fa2baa5f
rmfuhrer <>
2011-03-02 15:47:22 +0000

    Added tagging script

---
+++ lpg.update/tag-all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+usageMsg="Usage: $0 <tag>"
+
+while [ $# != 0 ]; do
+  case $1 in
+    --help)
+	  echo "$usageMsg"
+	  exit 0
+	  ;;
+    *)
+	  tag=$1
+	  ;;
+  esac
+  shift
+done
+
+if [ -z "$tag" ]; then
+    echo "No tag specified!"
+    echo "$usageMsg"
+    exit 1
+fi
+
+projects="lpg.generator lpg.generator.cpp lpg.generator.feature lpg.runtime lpg.runtime.java lpg.runtime.feature lpg.update"
+
+echo cvs rtag -r HEAD $tag $projects
