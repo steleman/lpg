c163453f49e8d30c5fa3ef28f67bb73ac67d8d20
rmfuhrer <>
2011-03-21 15:42:48 +0000

    First partially-working version of new options mechanism.

---
+++ lpg.generator.cpp/src/options.cpp
@@ -0,0 +1,622 @@
+/*
+ *  options.cpp
+ *  lpg
+ *
+ *  Created by Robert M. Fuhrer on 3/13/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "options.h"
+
+std::list<OptionDescriptor*> OptionDescriptor::allOptionDescriptors;
+
+OptionDescriptor::OptionDescriptor(OptionType t, const char *wd1, OptionProcessor::ValueHandler handler, bool valueOpt)
+: type(t), word1(wd1), word2(NULL), valueOptional(valueOpt), valueHandler(handler)
+{
+    setupName();
+    allOptionDescriptors.push_back(this);
+}
+
+OptionDescriptor::OptionDescriptor(OptionType t, const char *wd1, const char *wd2, OptionProcessor::ValueHandler handler, bool valueOpt)
+: type(t), word1(wd1), word2(wd2), valueOptional(valueOpt), valueHandler(handler)
+{
+    setupName();
+    allOptionDescriptors.push_back(this);
+}
+
+void
+OptionDescriptor::setupName()
+{
+    name = word1;
+    if (word2 != NULL) {
+        name.append("-");
+        name.append(word2);
+    }
+}
+
+const std::list<OptionDescriptor*>&
+OptionDescriptor::getAllDescriptors()
+{
+    return allOptionDescriptors;
+}
+
+EnumOptionDescriptor::EnumOptionDescriptor(const char *wd1, const char *enumValues, OptionProcessor::ValueHandler handler)
+: OptionDescriptor(ENUM, wd1, handler, false)
+{
+    setupName();
+    setupEnumValues(enumValues);
+}
+
+EnumOptionDescriptor::EnumOptionDescriptor(const char *wd1, const char *wd2, const char *enumValues, OptionProcessor::ValueHandler handler)
+: OptionDescriptor(ENUM, wd1, wd2, handler, false)
+{
+    setupName();
+    setupEnumValues(enumValues);
+}
+
+void
+EnumOptionDescriptor::setupEnumValues(const char *enumValues)
+{
+    char *copy = new char[strlen(enumValues)+1];
+    strcpy(copy, enumValues);
+    char *pStart = copy;
+    do {
+        char *pEnd = strchr(pStart, '|');
+        std::string *val;
+        
+        if (pEnd == NULL) {
+            val = new std::string(pStart);
+            pStart = pEnd;
+        } else {
+            val = new std::string(pStart, pEnd - pStart);
+            pStart = pEnd+1;
+        }
+        
+        legalValues.push_back(*val);
+        delete val;
+    } while (pStart != NULL);
+    delete[] copy;
+}
+
+void
+OptionDescriptor::processSetting(OptionProcessor *processor, OptionValue *v)
+{
+    OptionDescriptor *od = v->getOptionDescriptor();
+
+    cerr << "Setting option '" << od->getName() << "' to value " << *v->toString() << endl;
+
+    (processor->*valueHandler)(v);
+}
+
+OptionValue *
+OptionDescriptor::createValue()
+{
+    switch (getType()) {
+        case BOOLEAN: {
+            BooleanOptionValue *bv = new BooleanOptionValue(this);
+            return bv;
+        }
+        case ENUM: {
+            EnumOptionValue *ev = new EnumOptionValue(this);
+            return ev;
+        }
+        case INTEGER: {
+            IntegerOptionValue *iv = new IntegerOptionValue(this);
+            return iv;
+        }
+        case PATH: {
+            PathOptionValue *pv = new PathOptionValue(this);
+            return pv;
+        }
+        case STRING: {
+            StringOptionValue *sv = new StringOptionValue(this);
+            return sv;
+        }
+        case STRING_LIST: {
+            StringListOptionValue *slv = new StringListOptionValue(this);
+            return slv;
+        }
+        case PATH_LIST: {
+            PathListOptionValue *plv = new PathListOptionValue(this);
+            return plv;
+        }
+        default:
+            return NULL; // shouldn't happen
+    }
+}
+
+OptionProcessor::OptionProcessor(Option *option)
+: options(option)
+{ }
+
+void
+OptionProcessor::processAnyOption(OptionValue *v)
+{
+    // no-op
+}
+
+std::string
+trimQuotes(std::string& s)
+{
+    std::string result;
+//  cerr << "Pre-trimmed: " << s << endl;
+    int begin= 0, len = s.length();
+    while (s[begin] == ' ') {
+        begin++; len--;
+    }
+    if (s[begin] == '"') {
+        begin++; len--;
+    }
+    if (s[begin+len-1] == '"') {
+        len--;
+    }
+    result += s.substr(begin, len);
+//  cerr << "Post-trimmed: " << result << endl;
+    return result;
+}
+
+OptionDescriptor *actionBlock = new OptionDescriptor(STRING_LIST, "action", "block", &OptionProcessor::processActionBlock);
+
+void
+OptionProcessor::processActionBlock(OptionValue *v)
+{
+    StringListOptionValue *slv = static_cast<StringListOptionValue*> (v);
+    std::list<std::string> values = slv->getValue();
+    std::list<std::string>::iterator i = values.begin();
+    std::string fileName = trimQuotes(*i++);
+    std::string blockBegin = trimQuotes(*i++);
+    std::string blockEnd = trimQuotes(*i++);
+
+//  cerr << "action-block filename:    " << fileName << endl;
+//  cerr << "action-block block-begin: " << blockBegin << endl;
+//  cerr << "action-block block-end:   " << blockEnd << endl;
+    Option::BlockInfo& actionBlock = options->action_options.Next();
+    actionBlock.Set(NULL, strdup(fileName.c_str()), strdup(blockBegin.c_str()), strdup(blockEnd.c_str()));
+}
+
+OptionDescriptor *astDirectory = new OptionDescriptor(PATH, "ast", "directory", &OptionProcessor::processASTdirectory, true);
+
+void
+OptionProcessor::processASTdirectory(OptionValue *v)
+{
+    StringOptionValue *sv = static_cast<StringOptionValue*> (v);
+    options->ast_directory = strdup(sv->getValue().c_str());
+}
+
+OptionDescriptor *astType = new OptionDescriptor(STRING, "ast", "type", &OptionProcessor::processASTtype);
+
+void
+OptionProcessor::processASTtype(OptionValue *v)
+{
+    StringOptionValue *sv = static_cast<StringOptionValue*> (v);
+    
+    options->ast_type = sv->getValue().c_str();
+}
+
+OptionDescriptor *attributes = new OptionDescriptor(STRING, "attributes", &OptionProcessor::processAttributes, true);
+
+void
+OptionProcessor::processAttributes(OptionValue *v)
+{
+    BooleanOptionValue *bv = static_cast<BooleanOptionValue*> (v);
+    options->attributes = bv->getValue();
+}
+
+OptionDescriptor *automaticAST = new EnumOptionDescriptor("automatic", "ast", "none|nested|toplevel", &OptionProcessor::processAutomaticAST);
+
+void
+OptionProcessor::processAutomaticAST(OptionValue *v)
+{
+    EnumOptionValue *ev = static_cast<EnumOptionValue*> (v);
+    
+    if (!ev->getValue().compare("nested")) {
+        options->automatic_ast = Option::NESTED;
+    } else if (!ev->getValue().compare("toplevel")) {
+        options->automatic_ast = Option::TOPLEVEL;
+    } else {
+        options->automatic_ast = Option::NONE;
+    }
+}
+
+OptionDescriptor *debug = new OptionDescriptor(BOOLEAN, "debug", &OptionProcessor::processDebug, true);
+
+void
+OptionProcessor::processDebug(OptionValue *v)
+{
+    BooleanOptionValue *bv = static_cast<BooleanOptionValue*> (v);
+    options->debug = bv->getValue();
+}
+
+OptionDescriptor *includeDirs = new OptionDescriptor(PATH_LIST, "include", "directory", &OptionProcessor::processIncludeDir, false);
+
+void
+OptionProcessor::processIncludeDir(OptionValue *v)
+{
+    PathListOptionValue *plv = static_cast<PathListOptionValue*> (v);
+    std::list<std::string> values = plv->getValue();
+
+//  options->include_search_directory.Reset();
+    for(std::list<std::string>::iterator i= values.begin(); i != values.end(); i++) {
+        std::string path = *i;
+//      cerr << "include-dir path: " << path << endl;
+        options->include_search_directory.Next() = strdup(path.c_str());
+    }
+}
+
+OptionDescriptor *lalr = new OptionDescriptor(INTEGER, "lalr", &OptionProcessor::processLALR, false);
+
+void
+OptionProcessor::processLALR(OptionValue *v)
+{
+    IntegerOptionValue *iv = static_cast<IntegerOptionValue*> (v);
+    options->lalr_level = iv->getValue();
+}
+
+OptionDescriptor *names = new EnumOptionDescriptor("names", "optimized|minimum|maximum", &OptionProcessor::processNames);
+
+void
+OptionProcessor::processNames(OptionValue *v)
+{
+    EnumOptionValue *ev = static_cast<EnumOptionValue*> (v);
+    std::string value = ev->getValue();
+
+    if (!value.compare("optimized")) {
+        options->names = Option::OPTIMIZED;
+    } else if (!value.compare("minimum")) {
+        options->names = Option::MINIMUM;
+    } else if (!value.compare("maximum")) {
+        options->names = Option::MAXIMUM;
+    }
+}
+
+OptionDescriptor *softKeywords = new OptionDescriptor(BOOLEAN, "soft", "keywords", &OptionProcessor::processSoftKeywords, true);
+
+void
+OptionProcessor::processSoftKeywords(OptionValue *v)
+{
+    BooleanOptionValue *bv = static_cast<BooleanOptionValue*> (v);
+    options->soft_keywords = bv->getValue();
+}
+
+OptionDescriptor *variables = new EnumOptionDescriptor("variables", "none|both|terminals|nt|nonterminals", &OptionProcessor::processVariables);
+
+void
+OptionProcessor::processVariables(OptionValue *v)
+{
+    EnumOptionValue *ev = static_cast<EnumOptionValue*> (v);
+    std::string value = ev->getValue();
+    
+    if (!value.compare("none")) {
+        options->variables = Option::NONE;
+    } else if (!value.compare("both")) {
+        options->variables = Option::BOTH;
+    } else if (!value.compare("terminals")) {
+        options->variables = Option::TERMINALS;
+    } else if (!value.compare("nonterminals") || !value.compare("nt")) {
+        options->variables = Option::NON_TERMINALS;
+    }
+}
+
+OptionDescriptor *
+OptionValue::getOptionDescriptor()
+{
+    return optionDesc;
+}
+
+void
+StringListOptionValue::addValue(const char *s)
+{
+    std::string v = s;
+    values.push_back(v);
+}
+
+OptionParser::OptionParser(const std::list<OptionDescriptor*> opts)
+{
+    allOptions = opts;
+}
+
+bool
+OptionParser::IsDelimiter(char c)
+{
+    return (c == NULL_CHAR || c == ',' || Code::IsSpace(c));
+}
+
+OptionDescriptor *
+OptionParser::findOption(const char *&start)
+{
+    for (std::list<OptionDescriptor*>::iterator i=allOptions.begin(); i != allOptions.end(); i++) {
+        OptionDescriptor *od = *i;
+        const char *p = start;
+        const char *word1 = od->getWord1();
+        const char *word2 = od->getWord2();
+        
+        if ((p[0] == *word1 || p[0] == ToUpper(*word1)) &&
+            (word2 != NULL && (p[1] == *word2 || p[1] == ToUpper(*word2)) &&
+             (p[2] == '=' || IsDelimiter(p[2])) ||
+             word2 == NULL && (p[2] == '=' || IsDelimiter(p[2])))) {
+                start += 2;
+                return od;
+            }
+        
+        int length1 = strlen(word1);
+        int length = length1 + strlen(word2) + 1; // +1 for separator
+        char *name = new char[length + 1];
+        
+        strcpy(name, word1);
+        strcat(name, word2);
+        int i = strxsub(p, name) - 1;
+        
+        strcpy(name, word1);
+        strcat(name, "_");
+        strcat(name, word2);
+        
+        i = Max(i, strxsub(p, name) - 1);
+        
+        name[length1] = '-';
+        
+        i = Max(i, strxsub(p, name) - 1);
+        
+        delete [] name;
+        
+        if (start[i+1] == '=' || IsDelimiter(start[i+1])) {
+            start += i + 1;
+            return od;
+        }
+    }
+    return NULL;
+}
+
+void
+BooleanOptionValue::parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException)
+{
+    if (v == NULL) {
+        if (od->isValueOptional()) {
+            value = true;
+        } else {
+            throw ValueFormatException("Missing boolean value", od);
+        }
+    } else if (!v->compare("true")) {
+        value = true;
+    } else if (!v->compare("false")) {
+        value = false;
+    } else {
+        throw ValueFormatException("Invalid boolean value", *v, od);
+    }
+}
+
+static const std::string *TRUE_VALUE_STR = new std::string("true");
+static const std::string *FALSE_VALUE_STR = new std::string("false");
+
+const std::string *
+BooleanOptionValue::toString()
+{
+    return value ? TRUE_VALUE_STR : FALSE_VALUE_STR;
+}
+
+void
+IntegerOptionValue::parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException)
+{
+    if (v == NULL) {
+        if (od->isValueOptional()) {
+            value = 0;
+        } else {
+            throw ValueFormatException("Missing integer value", od);
+        }
+    }
+    
+    const char *vs = v->c_str();
+    
+    if (!verify(vs)) {
+        throw ValueFormatException("Invalid integer value", *v, od);
+    } else {
+        value = atoi(vs);
+    }
+}
+
+const std::string *
+IntegerOptionValue::toString()
+{
+    IntToString i2s(value);
+    std::string *result = new std::string(i2s.String());
+    return result;
+}
+
+void
+StringOptionValue::parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException)
+{
+    if (v == NULL) {
+        throw ValueFormatException("Missing string value", od);
+    }
+    if (v->length() == 0) {
+        value = *v;
+    } else if (v->at(0) == '\'') {
+        if (v->at(v->length()-1) != '\'') {
+            throw ValueFormatException("String option value must be quoted", *v, od);
+        }
+        // trim quotes
+        value = v->substr(1, v->length() - 2);
+    } else {
+        value = v->substr(0, v->length());
+    }
+}
+
+const std::string *
+StringOptionValue::toString()
+{
+    std::string *result = new std::string(value.c_str());
+    return result;
+}
+
+void
+EnumOptionValue::parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException)
+{
+    if (v == NULL) {
+        throw ValueFormatException("Missing enum value", od);
+    }
+    
+    int endIdx = v->find_first_of(',');
+    
+    std::string enumValue = v->substr(0, endIdx);
+    
+    EnumOptionDescriptor *eod = static_cast<EnumOptionDescriptor*> (od);
+    
+    // Check that the given value is one of the allowed values
+    std::list<std::string> legalValues = eod->getLegalValues();
+    
+    bool found = false;
+    for (std::list<std::string>::iterator i = legalValues.begin(); i != legalValues.end(); i++) {
+        if (!(*i).compare(enumValue)) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        // TODO say what are the legal values
+        std::string msg;
+        msg.append("Legal values are: {");
+        for (std::list<std::string>::iterator i = legalValues.begin(); i != legalValues.end(); i++) {
+            if (i != legalValues.begin()) {
+                msg.append(" |");
+            }
+            msg.append(" ");
+            msg.append(*i);
+        }        
+        msg.append(" }");
+        throw ValueFormatException(msg, *v, od);
+    }
+    value = *v;
+}
+
+void
+StringListOptionValue::parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException)
+{
+    if (v == NULL) {
+        throw ValueFormatException("Missing list-of-strings value", od);
+    }
+    
+    // Trim surrounding ()'s, and split string at commas
+    // Work on a copy
+    const char *p = v->c_str();
+    if (p[0] != '(') {
+        throw ValueFormatException("String-list-valued option must be enclosed in parentheses", *v, od);
+    }
+    if (p[strlen(p)-1] != ')') {
+        throw ValueFormatException("String-list-valued option must be enclosed in parentheses", *v, od);
+    }
+    char *pCopy = strdup(p+1); // trim leading '('
+    pCopy[strlen(pCopy)-1] = '\0'; // trim trailing ')'
+    
+    const char *pStart = pCopy;
+    do {
+        char *pEnd = strchr(pStart, ',');
+        std::string *val;
+        
+        if (pEnd == NULL) {
+            val = new std::string(pStart);
+            pStart = pEnd;
+        } else {
+            val = new std::string(pStart, pEnd - pStart);
+            pStart = pEnd + 1;
+        }
+        
+        values.push_back(*val);
+        delete val;
+    } while (pStart != NULL);
+
+    free(pCopy);
+}
+
+const std::string *
+StringListOptionValue::toString()
+{
+    std::string *result = new std::string();
+    
+    for (std::list<std::string>::iterator i = values.begin(); i != values.end(); i++) {
+        if (i != values.begin()) {
+            result->append(",");
+        }
+        result->append(*i);
+    }
+    return result;
+}
+
+void
+PathListOptionValue::parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException)
+{
+    if (v == NULL) {
+        throw ValueFormatException("Missing path-list value", od);
+    }
+    
+    // Split string at semicolons
+    const char *pStart = v->c_str();
+    do {
+        char *pEnd = strchr(pStart, ';');
+        std::string *val;
+        
+        if (pEnd == NULL) {
+            val = new std::string(pStart);
+            pStart = pEnd;
+        } else {
+            val = new std::string(pStart, pEnd - pStart);
+            pStart = pEnd + 1;
+        }
+        
+        values.push_back(*val);
+        delete val;
+    } while (pStart != NULL);
+}
+
+const std::string *
+PathListOptionValue::toString()
+{
+    std::string *result = new std::string();
+    
+    for (std::list<std::string>::iterator i = values.begin(); i != values.end(); i++) {
+        if (i != values.begin()) {
+            result->append(";");
+        }
+        result->append(*i);
+    }
+    return result;
+}
+
+std::string *
+OptionParser::getOptionValue(const char *&p)
+{
+    if (p != NULL && *p == '=') {
+        int plen = strlen(p+1);
+        const char *delim;
+        
+        if (p[1] == '(') {
+            // A parenthesized string list
+            delim = strchr(p, ')');
+            delim = strchr(delim, ',');
+        } else {
+            delim = strchr(p, ',');
+        }
+
+        int len = (delim != NULL) ? delim - p - 1 : plen;
+        std::string *result = new std::string(p+1, len);
+
+        p += result->length() + 1;
+        return result;
+    }
+    return NULL;
+}
+
+OptionValue *
+OptionParser::parse(const char *&start) throw(ValueFormatException)
+{
+    OptionDescriptor *od = findOption(start);
+    
+    if (od != NULL) {
+        // This option is a match
+        std::string *optValueStr = getOptionValue(start);
+        OptionValue *optValue = od->createValue();
+        
+        optValue->parseValue(optValueStr, od);
+        return optValue;
+    }
+    return NULL;
+}
---
+++ lpg.generator.cpp/src/options.h
@@ -0,0 +1,224 @@
+#ifndef OPTIONS_INCLUDED
+#define OPTIONS_INCLUDED
+
+/*
+ *  options.h
+ *  lpg
+ *
+ *  Created by Robert M. Fuhrer on 3/13/11.
+ *  Copyright 2011 . All rights reserved.
+ */
+
+#include "option.h"
+
+enum OptionType {
+    BOOLEAN,
+    ENUM,
+    INTEGER,
+    STRING,
+    STRING_LIST,
+    PATH,
+    PATH_LIST
+};
+
+class OptionValue;
+
+class OptionProcessor {
+public:
+    typedef void (OptionProcessor::*ValueHandler)(OptionValue *);
+    
+    OptionProcessor(Option *);
+    
+    void processAnyOption(OptionValue *v);
+
+    void processActionBlock(OptionValue *v);
+    void processASTdirectory(OptionValue *v);
+    void processASTtype(OptionValue *v);
+    void processAttributes(OptionValue *v);
+    void processAutomaticAST(OptionValue *v);
+    void processDebug(OptionValue *v);
+    void processIncludeDir(OptionValue *v);
+    void processLALR(OptionValue *v);
+    void processNames(OptionValue *v);
+    void processSoftKeywords(OptionValue *v);
+    void processVariables(OptionValue *v);
+    
+private:
+    Option *options;
+};
+
+class OptionDescriptor {
+public:
+    OptionDescriptor(OptionType type, const char *word1, OptionProcessor::ValueHandler handler, bool valueOptional = false);
+    OptionDescriptor(OptionType type, const char *word1, const char *word2, OptionProcessor::ValueHandler handler, bool valueOptional = false);
+    
+    const char *getWord1() const { return word1; }
+    const char *getWord2() const { return (word2 != NULL) ? word2 : ""; }
+    const std::string& getName() const { return name; }
+    OptionType getType() const { return type; }
+    bool isValueOptional() const { return valueOptional; }
+    
+    OptionValue *createValue();
+    
+    void processSetting(OptionProcessor *processor, OptionValue *value);
+
+    static const std::list<OptionDescriptor*>& getAllDescriptors();
+
+    static std::string describeAllOptions();
+
+protected:
+    void setupName();
+    
+    const OptionType type;
+    const char *word1;
+    const char *word2; // may be null
+    std::string name;
+    const bool valueOptional;
+    OptionProcessor::ValueHandler valueHandler;
+
+    static std::list<OptionDescriptor*> allOptionDescriptors;
+};
+
+class EnumOptionDescriptor : public OptionDescriptor {
+public:
+    EnumOptionDescriptor(const char *word1, const char *enumValues, OptionProcessor::ValueHandler handler);
+    EnumOptionDescriptor(const char *word1, const char *word2, const char *enumValues, OptionProcessor::ValueHandler handler);
+    
+    const std::list<std::string>& getLegalValues() const { return legalValues; }
+    
+private:
+    void setupEnumValues(const char *);
+    
+    std::list<std::string> legalValues;
+};
+
+class ValueFormatException {
+public:
+    ValueFormatException(const char *msg, OptionDescriptor *od)
+    : msg(msg), valueStr(NULL), optDesc(od)
+    { }
+    
+    ValueFormatException(const char *msg, std::string& s, OptionDescriptor *od)
+    : msg(msg), valueStr(s), optDesc(od)
+    { }
+    
+    ValueFormatException(std::string& msg, std::string& s, OptionDescriptor *od)
+    : msg(msg), valueStr(s), optDesc(od)
+    { }
+    
+    const std::string& message() { return msg; }
+    const std::string& value() { return valueStr; }
+    const OptionDescriptor *optionDescriptor() { return optDesc; }
+    
+private:
+    std::string msg;
+    std::string valueStr;
+    OptionDescriptor *optDesc;
+};
+
+class OptionValue : public Code, public Util {
+public:
+    OptionDescriptor *getOptionDescriptor();
+    
+    virtual void parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException) = 0;
+    virtual const std::string *toString() = 0;
+    
+protected:
+    OptionValue(OptionDescriptor *od) : optionDesc(od) { }
+    
+private:
+    OptionDescriptor *optionDesc;
+};
+
+class BooleanOptionValue : public OptionValue {
+public:
+    BooleanOptionValue(OptionDescriptor *od) : OptionValue(od) { }
+    
+    void setValue(bool v) { value = v; }
+    bool getValue() { return value; }
+    
+    void parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException);
+    const std::string *toString();
+    
+private:
+    bool value;
+};
+
+class IntegerOptionValue : public OptionValue {
+public:
+    IntegerOptionValue(OptionDescriptor *od) : OptionValue(od) { }
+    
+    void setValue(int v) { value = v; }
+    int getValue() { return value; }
+    
+    void parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException);
+    const std::string *toString();
+
+private:
+    int value;
+};
+
+class StringOptionValue : public OptionValue {
+public:
+    StringOptionValue(OptionDescriptor *od) : OptionValue(od) { }
+
+    void setValue(const char *v) { value = v; }
+    const std::string& getValue() { return value; }
+
+    void parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException);
+    const std::string *toString();
+
+protected:
+    std::string value;
+};
+
+class EnumOptionValue : public StringOptionValue {
+public:
+    EnumOptionValue(OptionDescriptor *od) : StringOptionValue(od) { }
+
+    void parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException);
+};
+
+class PathOptionValue : public StringOptionValue {
+public:
+    PathOptionValue(OptionDescriptor *od) : StringOptionValue(od) { }
+};
+
+class StringListOptionValue : public OptionValue {
+public:
+    StringListOptionValue(OptionDescriptor *od) : OptionValue(od) { }
+
+    const std::list<std::string>& getValue() { return values; }
+    void addValue(const char *v);
+    void addValues(const StringListOptionValue &other);
+
+    void parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException);
+    const std::string *toString();
+
+protected:
+    std::list<std::string> values;
+};
+
+class PathListOptionValue : public StringListOptionValue {
+public:
+    PathListOptionValue(OptionDescriptor *od) : StringListOptionValue(od) { }
+
+    void parseValue(std::string *v, OptionDescriptor *od) throw(ValueFormatException);
+    const std::string *toString();
+};
+
+class OptionParser : public Code, public Util {
+public:
+    OptionParser(const std::list<OptionDescriptor*> descriptors);
+
+    OptionValue *parse(const char *&start) throw(ValueFormatException);
+
+private:
+    OptionDescriptor *findOption(const char *&start);
+    bool IsDelimiter(char c);
+    std::string *getOptionValue(const char *&p);
+    
+    std::list<OptionDescriptor*> allOptions;
+};
+
+#endif
