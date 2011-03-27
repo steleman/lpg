ecffbf54e7d589d68e4dffeab9cb60c5bfd41849
rmfuhrer <>
2011-03-27 22:00:17 +0000

    Removed virtual method parser(), which was unused and prevented instantiation. Renamed method getIToken() to getToken(); it was presumably copied from Java runtime. dump() now prints start/end offsets.

--- lpg.runtime.cpp/lpg/runtime/PrsStream.h
+++ lpg.runtime.cpp/lpg/runtime/PrsStream.h
@@ -6,30 +6,34 @@
 
 class PrsStream
 {
-protected:
-    Tuple<Token> tokens_;
-    Tuple<Token> adjuncts_;
-    int index_;
 public:
     PrsStream() : tokens_(12, 16), adjuncts_(12, 16) {}
     virtual ~PrsStream() {}
-    virtual void* parser() =0;
+
     inline int getNext(int i)
       { return (++i < tokens_.Length() ? i : tokens_.Length() - 1); }
+
     inline int getPrevious(int i) 
       { return (i <= 0 ? 0 : i - 1); }
+
     inline int peek() 
       { return getNext(index_); }
+
     inline void reset(int i = 1) 
       { index_ = getPrevious(i); }
+
     inline int getToken()
       { return index_ = getNext(index_); }
+
     inline int getToken(int end_token)
       { return index_ = (index_ < end_token ? getNext(index_) : tokens_.Length() - 1); }
+
     inline int badToken()
       { return 0; }
+
     inline unsigned getKind(int i)
       { return tokens_[i].getKind(); }
+
     inline void resetTokenStream(int size = 0)
       { tokens_.Reset(size); }
 
@@ -50,15 +54,22 @@ public:
     {
         printf("TOKENS(%d)\n", tokens_.Length());
         for (int i = 0; i < tokens_.Length(); i++) {
-            printf("% 4d|[% 4d] %s\n", i, 
+            printf("% 4d [%d..%d] : [% 4d] %s\n", i, 
+		   tokens_[i].getStartOffset(),
+		   tokens_[i].getEndOffset(),
                    tokens_[i].getKind(),
                    tokens_[i].getValue());
         }
     }
 
-    inline Token* getIToken(int i)
+    inline Token* getToken(int i)
     {
         return &tokens_[i];
     }
+
+protected:
+    Tuple<Token> tokens_;
+    Tuple<Token> adjuncts_;
+    int index_;
 };
 #endif
