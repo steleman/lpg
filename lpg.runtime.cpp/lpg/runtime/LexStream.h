d86b78dd528536e465f5d2a03821b0b71a082713
rmfuhrer <>
2011-04-07 13:16:10 +0000

    Ctor now saves the filename in a field, and added a getter for it. Added various methods to roughly conform to the API of the Java version of this class expected by the lexer class impl.

--- lpg.runtime.cpp/lpg/runtime/LexStream.h
+++ lpg.runtime.cpp/lpg/runtime/LexStream.h
@@ -10,11 +10,15 @@
 class LexStream
 {
 protected:
-    PrsStream *prsStream_;
-    char      *inputChars_;
+    PrsStream  *prsStream_;
+    const char *fileName_;
+    const char *inputChars_;
+    int        streamLength_;
+    int        index_;
+
 public:
     LexStream(const char* fileName) :
-        prsStream_(NULL)
+        prsStream_(NULL), fileName_(fileName)
     {
         FILE*  fp;
         size_t size, n;
@@ -25,16 +29,89 @@ public:
         size = ftell(fp); 
         fseek(fp, 0, SEEK_SET);
 
-        inputChars_ = new char[size+4];
-        n = fread(inputChars_, 1, size, fp);
-        inputChars_[n] = 0xff; inputChars_[n+1] = 0;
+        int charLen = size + 3;
+        char *tmp = new char[charLen+1];
+        n = fread(tmp, 1, size, fp);
+        tmp[n] = 0xff; tmp[n+1] = 0;
+        streamLength_ = charLen;
         fclose(fp);
+        inputChars_ = tmp;
     }
+
     virtual ~LexStream() { delete[] inputChars_; }
-    virtual void ruleAction(int n) {};
+
+    virtual void ruleAction(int n) {}
+
     virtual int lexer(PrsStream* ps) =0;
-    inline char* getInputChars()
-      { return inputChars_; }
+
+    void setInputChars(const char *chars)
+    {
+        inputChars_ = chars;
+        index_ = -1;
+        streamLength_ = strlen(inputChars_);
+    }
+
+    inline const char* getInputChars()
+    {
+        return inputChars_;
+    }
+
+    inline int getInputLength()
+    {
+        return streamLength_;
+    }
+
+    const char *getFileName()
+    {
+        return fileName_;
+    }
+
+    inline void setStreamIndex(int index)
+    {
+        index_ = index;
+    }
+
+    inline int getStreamIndex()
+    {
+        return index_;
+    }
+
+    inline void setStreamLength(int len)
+    {
+        streamLength_ = len;
+    }
+
+    inline int getStreamLength()
+    {
+        return streamLength_;
+    }
+
+    inline int getCharValue(int idx)
+    {
+        return inputChars_[idx];
+    }
+
+    inline int getToken()
+    {
+        return index_ = getNext(index_);
+    }
+    
+    inline int getToken(int end_token)
+    {
+        return index_ = (index_ < end_token ? getNext(index_) : streamLength_);
+    }
+    
+//    virtual int getKind(int i) = 0;
+    
+    int getNext(int i) { return (++i < streamLength_ ? i : streamLength_); }
+    
+    int getPrevious(int i) { return (i <= 0 ? 0 : i - 1); }
+    
+    int peek() { return getNext(index_); }
+    
+    void reset(int i) { index_ = i - 1; }
+    
+    void reset() { index_ = -1; }
 
     inline void makeToken(int startLoc, int endLoc, int kind)
     {
@@ -46,7 +123,15 @@ public:
         prsStream_->makeAdjunct(inputChars_, startLoc, endLoc, kind);
     }
 
+    void reportLexicalError(int left_loc, int right_loc)
+    {
+        const char *msg = (right_loc >= streamLength_) ? "End-of-file" : "Invalid token";
+        std::cerr << fileName_ << ", line " << left_loc << ", column " << right_loc << ": " << msg <<  std::endl;
+    }
+
     inline void dump()
-    { prsStream_->dump(); }
+    {
+        prsStream_->dump();
+    }
 };
 #endif
