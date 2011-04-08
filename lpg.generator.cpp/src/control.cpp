d0518eee7b1fb0a0fab8eedd5d36213b50167328
rmfuhrer <>
2011-04-08 19:39:26 +0000

    Bumped version # to 2.0.23

--- lpg.generator.cpp/src/control.cpp
+++ lpg.generator.cpp/src/control.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 const char Control::HEADER_INFO[]  = "IBM LALR Parser Generator",
-           Control::VERSION[] = "2.0.22 (" __DATE__ ")";
+           Control::VERSION[] = "2.0.23 (" __DATE__ ")";
 
 //
 //
