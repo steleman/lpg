d94f91b874a62639820dce1187f30da68a709a99
rmfuhrer <>
2011-04-25 17:08:56 +0000

    Added test inputs for exercising LPG on the generator grammar itself.

---
+++ lpg.test/tests/lpg/Main.java
@@ -0,0 +1,42 @@
+package lpg;
+
+import org.eclipse.imp.lpg.parser.LPGParser;
+import org.eclipse.imp.lpg.parser.LPGLexer;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Option option;
+        LPGLexer lpg_lexer;
+        LPGParser lpg_parser;
+
+        try
+        {
+            option = new Option(args);
+            lpg_lexer = new LPGLexer(option.getFileName()); // Create the lexer
+            lpg_parser = new LPGParser(lpg_lexer.getILexStream()); // Create the parser
+            lpg_lexer.lexer(lpg_parser.getIPrsStream()); // Lex the stream to produce the token stream
+            if (option.dumpTokens())
+            {
+                System.out.println("\n****Output Tokens: \n");
+                lpg_parser.getIPrsStream().dumpTokens();
+            }
+            Object ast = lpg_parser.parser(); // Parse the token stream to produce an AST
+            if (ast == null) 
+                 System.out.println("****Failure");
+            //else
+            //{
+                //Integer result = (Integer) ast.accept(new ExprResultVisitor());
+                //System.out.println(ast.toString() + " = " + result.intValue());
+            //}
+
+            return;
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
